b5bdc6d51911cde9be91cb0918a935eea066a19e
gijack2447 <jackiegg773@gmail.com>
2023-02-28 13:21:33 -0600

    mods

--- file_reader.py
+++ file_reader.py
@@ -7,3 +7,11 @@ filename = 'text_files/pi_digits.txt'
 with open(filename) as file_object:
     for line in file_object:
         print(line.rstrip())
+# rstrip() strips whitespace to the right of lines
+
+"""Making a List of Lines from a File"""
+with open(filename) as file_object:
+    lines = file_object.readlines()
+    # readlines() takes each line from file and stores in a list (lines)
+for line in lines:
+    print(line.rstrip())
