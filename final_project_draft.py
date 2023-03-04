68898f0d168c3b8a4c1ed5ab1adc1cf29cde99ee
gijack2447 <jackiegg773@gmail.com>
2023-03-04 09:52:02 -0600

    main function

--- final_project_draft.py
+++ final_project_draft.py
@@ -2,12 +2,23 @@ import json, requests
 
 base_url = "https://api.openweathermap.org/data/2.5/weather"
 apikey = "134a631032499d98cc8d99fcfdcacbf7"
-city = "Omaha"
 
-url = f"{base_url}?q={city}&units=imperial&APPID={apikey}"
-print(url)
+def main():
+    """Main function to accept input and return weather data"""
+    while True:
+        city = input("\nInput a city, zip code, or type 'q' to quit: ")
 
-response = requests.get(url)
-json_data = response.json()
+        if city == 'q':
+            quit()
+        else:
+            url = f"{base_url}?q={city}&units=imperial&APPID={apikey}"         
+            response = requests.get(url)
+            json_data = response.json()
 
-print(json_data)
+            temp = json_data['main']['temp']
+            print(f"The current temperature in {city.title()} is: {temp}")
+
+            max_temp = json_data['main']['temp_max']
+            print(f"The max temperature today in {city.title()} is: {max_temp}")
+
+main()
