59a23fc7e25d0f2ae7d29cf3b7125bdfd9046dd0
gijack2447 <jackiegg773@gmail.com>
2023-03-06 10:01:09 -0600

    added log file for tzeentch

--- dnslookup.py
+++ dnslookup.py
@@ -6,16 +6,19 @@ def nslook():
 
         # Finding A record
         site = input("\nInput a domain name or press 'q' to quit: ")
-        if site == 'q':
-            quit()
-        # Query private server
-        if site == '40k':
-            site = ""
-        else:
-            site = dns.resolver.resolve(site, 'A')
+        with open('tzeentch.txt', 'a') as f:
+            if site == 'q':
+                quit()
+            # Query private server
+            if site == '40k':
+                site = ""
+            else:
+                dns_site = dns.resolver.resolve(site, 'A')
+                for val in dns_site:
+                    f.write(f"\n{site}: {val.to_text()}")
         
         # Printing record
-        for val in site:
+        for val in dns_site:
             print("\nAccessing The Warp...")
             time.sleep(1)
             print(f'Tzeencht says {val.to_text()}...')
