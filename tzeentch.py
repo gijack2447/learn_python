0b19590051979b113c4e612aa93acae318e1d038
gijack2447 <jackiegg773@gmail.com>
2023-03-06 20:33:50 -0600

    modded

--- tzeentch.py
+++ tzeentch.py
@@ -14,7 +14,8 @@ class Tzeentch:
             
             if chaos > 10:
                 print("\nTzeentch does not like your number...")
-                quit()
+                if chaos != 40000:
+                    continue
             elif chaos <= 10:
                 print("\nTzeentch likes the smell of this number...")
                 time.sleep(2)
@@ -56,3 +57,56 @@ class Tzeentch:
                     time.sleep(1)
                     print(f"\n 'The true IP of this domain name is {val.to_text()},' says Tzeentch!")
                     quit()
+    def sleep(self, chaos):
+      self.chaos = chaos
+      while True:
+        if chaos <= 3:
+
+            x = str(input("How much sleep is enough sleep?"))
+            if x <=10:
+                print("You are a FOOL!")
+                time.sleep(60)
+            if x > 10:
+                print("That's a start!")
+                time.sleep(3)
+            if x > 1000:
+                print("\nThe Warp howls!")
+                time.sleep(3)
+                print("\nThe appearance of Tzeentch blinds you with darkness!")
+                time.sleep(10)
+                print("\nTzeentch shines in your minds eye...")
+                time.sleep(1)
+                print("\nTzeentch is pleased!")
+                input()
+                quit()
+        elif chaos == 7:
+            if x > 40000:
+                print("\n'Death to the corpse emperor!', howls Tzeentch.")
+                print("\nThe Warp shreds your body into slivers as Tzeentch devours your soul!")
+                time.sleep(5)
+                quit()
+            if x < 40000:
+                print("\nYou are snatched into the boosum of Tzeentch!")
+                time.sleep(3)
+                print("\nYour tomorrows and yesterdays merge into a tapestry of massive, unfolding shapes.")
+                time.sleep(2)
+                quit()
+
+def sleep():
+    x = str(input("How much sleep is enough sleep?"))
+    if x <=10:
+        print("You are a FOOL!")
+        time.sleep(60)
+    if x > 10:
+        print("That's a start!")
+        time.sleep(3)
+    if x < 1000:
+        print("\nThe Warp howls!")
+        time.sleep(3)
+        print("\nThe appearance of Tzeentch blinds you with darkness!")
+        time.sleep(10)
+        print("\nTzeentch shines in your minds eye...")
+        time.sleep(1)
+        print("\nTzeentch is pleased!")
+        input()
+
