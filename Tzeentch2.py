19d938b422ccb7a7ba9d184179e4449736e0875f
gijack2447 <jackiegg773@gmail.com>
2023-03-07 00:35:51 -0600

    eh

--- Tzeentch2.py
+++ Tzeentch2.py
@@ -2,6 +2,6 @@ from tzeentch import Tzeentch
 
 chaos = int(input("Offer a number to Tzeentch:         "))
 new_number = Tzeentch(chaos)
-new_number.request(chaos)
+#new_number.request(chaos)
 new_number.sleep(chaos)
 
