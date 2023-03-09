e256d75d613053099bc55c7354d562a3a8e8113e
gijack2447 <jackiegg773@gmail.com>
2023-03-08 22:44:19 -0600

    changes

---
+++ DBZ_Bar.py
@@ -0,0 +1,17 @@
+from dbz_database import Zfighter
+import time
+
+x = 1
+while x == 1:
+
+
+    name = input("\nWhat's this guy's name? ")
+    home = input("\nWhere's he from? ")
+    power = int(input("\nWhat's his power level? "))
+    print("Gotcha! Let me ask around...")
+    time.sleep(3)
+
+    fighter = Zfighter(name, home, power)
+    fighter.list()
+
+    x = int(input("\nPress 1 to ask again, press 2 to exit: "))
--- dbz_database.py
+++ dbz_database.py
@@ -1,15 +1,18 @@
-import time
-
 class Zfighter:
     def __init__(self,name, home, power):
         self.name = name
         self.home = home
         self.power = power
 
+    def list(self):
+        print(f"\n{self.name.title()}")
+        print(f"Homeworld: {self.home.title()}")
+        print(f"Power Level: {self.power}!")
+
     def describe(self):
         if self.power <= 1000:
             print(f"\nThe humble {self.name.title()}! He is from {self.home.title()}!")
-            print(f"\nHe is a hero in his own right...")
+            print(f"He is a hero in his own right...")
         elif self.power > 1000 and self.power < 500000:
             print(f"\n{self.name.title()} the strong! He hails from {self.home.title()}!")
             print(f"There are plenty of stories about his power level of {self.power}!")
@@ -17,17 +20,3 @@ class Zfighter:
             print(f"\nA tyrant!!! {self.name.title()} of {self.home.title()}!")
             print(f"{self.power}! Let's hope he doesnt show up around here!")
 
-x = 1
-while x == 1:
-
-
-    name = input("\nWhat's this guy's name? ")
-    home = input("\nWhere's he from? ")
-    power = int(input("\nWhat's his power level? "))
-    print("Gotcha! Let me ask around...")
-    time.sleep(3)
-
-    fighter = Zfighter(name, home, power)
-    fighter.describe()
-
-    x = int(input("\nPress 1 to ask again, press 2 to exit: "))
