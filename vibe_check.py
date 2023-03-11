d78deccde8b7cda4570d1db2445adb30b7148f9b
gijack2447 <jackiegg773@gmail.com>
2023-03-10 20:54:28 -0600

    vibe

--- vibe_check.py
+++ vibe_check.py
@@ -2,6 +2,9 @@
 
 import time, random
 
+scrawnies = ['weak','fragile','hollow', 'weak-minded','lifeless']
+bigs =['buff', 'strong', 'mighty', 'hardy', 'steadfast', 'miraculous']
+
 class Text_to:
     def __init__main(self, name, power):
         self.name = name
@@ -10,24 +13,35 @@ class Text_to:
     def vibe_check(self, name, power):
         self.name = name
         self.power = power
-        scrawnies = ['weak','fragile','hollow']
-        bigs =['buff', 'strong', 'mighty']
-        scrawny = random.choice(scrawnies)
+        
+        
 
         if power < 1000000:
-            print(f"\n{name.title()}! A suckling!")
+            print(f"\n{name.title()}! The {random.choice(scrawnies)}!")
             time.sleep(1) 
-            print(f"\n{name.title()} the {scrawny}!")
+            print(f"\n{name.title()} the {random.choice(scrawnies)}!")
             time.sleep(1)
             print("\nAhahahahahahhahhahaaaaaa!")
             time.sleep(2)
-            print("\nVery well then")
-        elif power >= 1000000:
+            print("\nVery well then...")
+        if power >= 1000000:
             print(f"{name.title()}!")
             time.sleep(2)
             print(f"{name.title()} the {random.choice(bigs).title()}! ")
             time.sleep(4)
-            print(f"Did you hear it, oh {random.choice(bigs)} {name.title()}? The angels rejoice about how {random.choice(bigs)} you are!\n")
+            print(f"\nThe demons of Khorne {random.choice(bigs)} {name.title()}? The angels rejoice about how {random.choice(bigs)} you are!\n")
+        
+        khorne = open('text_files/list_of_Khorne', 'r+')
+        if power >= 1000000:
+            khorne.write(f"\n{name.title()} the {random.choice(bigs).title()}")
+            khorne.write(f"The library of Khorne recognizes the {random.choice(bigs)} of {name.title()}!\n")
+
+        if power < 1000000:
+            khorne.write(f"\nThe library of Khorne cares not for the {random.choice(scrawnies)} {name.title()}!\n")
+
+    
+
+
 
 
 
