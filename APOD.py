a416888b29a4cfb66a0322b7a9cb50ae671a06b6
gijack2447 <jackiegg773@gmail.com>
2023-07-22 14:59:35 -0500

    fin

--- APOD.py
+++ APOD.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import webbrowser
 from urllib.request import urlretrieve
 from pprint import PrettyPrinter
 pp = PrettyPrinter()
@@ -8,12 +10,14 @@ apiKey = 'KfO5HEevBmYG2rrA6Jd4S4XjaQx4GRODy9BMQi2p'
 
 def fetchAPOD():
   URL_APOD = "https://api.nasa.gov/planetary/apod"
-  date = '2020-01-22'
+  date = '2023-07-22'
   params = {
       'api_key':apiKey,
       'date':date,
       'hd':'True'
   }
-  response = requests.get(URL_APOD,params=params).json()
-  pp.pprint(response)
+  response = requests.get(URL_APOD,params=params)
+  json_data = json.loads(response.text)
+  image_url = json_data['hdurl']
+  webbrowser.open(image_url)
 fetchAPOD()
